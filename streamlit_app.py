a2aa87f0f390ad5a8c7c1083ef8d31f4f8658c37
Richard Awe <richard3d7@aol.com>
2023-08-03 00:37:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,32 +44,32 @@ if prompt := st.chat_input(placeholder="Ask me anything"):
         st.session_state.messages.append({"role": "assistant", "content": response})
         st.write(response)
          
-    formbtn = st.button("Like this?")
-    if "formbtn_state" not in st.session_state:
-        st.session_state.formbtn_state = False
+formbtn = st.button("Like this?")
+if "formbtn_state" not in st.session_state:
+    st.session_state.formbtn_state = False
         
-    if formbtn or st.session_state.formbtn_state:
-        st.session_state.formbtn_state = True
+if formbtn or st.session_state.formbtn_state:
+    st.session_state.formbtn_state = True
             
-        st.subheader("Contact us")
+    st.subheader("Contact us")
         # name = st.text_input("Name")
-        with st.form(key = 'user_info'):
-            st.write('Like this? Lets get you one')
+    with st.form(key = 'user_info'):
+        st.write('Like this? Lets get you one')
             
-            name = st.text_input(label="Name 📛")
-            email = st.text_input(label="Email 📧")
-            phone = st.text_input(label="Phone 📱")
+        name = st.text_input(label="Name 📛")
+        email = st.text_input(label="Email 📧")
+        phone = st.text_input(label="Phone 📱")
             
-            submit_form = st.form_submit_button(label="Subscribe", help="Click to subscribe!")
+        submit_form = st.form_submit_button(label="Subscribe", help="Click to subscribe!")
             
                 # Checking if all the fields are non empty
-            if submit_form:
-                st.write(submit_form)
+        if submit_form:
+            st.write(submit_form)
             
-                if name and email :
+            if name and email :
                         # add_user_info(id, name, age, email, phone, gender)
-                    st.success(f"ID:  \n Name: {name} \n Email: {email}"
+                st.success(f"ID:  \n Name: {name} \n Email: {email}"
                                 )
-                    else:
-                        st.warning("Please fill all the fields")
+                else:
+                    st.warning("Please fill all the fields")
         
