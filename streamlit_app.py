dd616c01e38a7cbef2e330c197a52694e926c8c8
Richard Awe <richard3d7@aol.com>
2023-08-03 00:47:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,10 +57,3 @@ contact_form = """
 """
 
 st.markdown(contact_form, unsafe_allow_html=True)
-
-#Use local CSS file
-def local_css(file_name):
-     with open(file_name) as f:
-          st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-local_css("Contact_form/style/style.css")
